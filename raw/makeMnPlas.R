de62b4fbde1d462b642a6c822dec943a75edd856
stuartWagenius <s-wagenius@northwestern.edu>
2025-06-03 12:09:24 -0500

    add makeMnPlas.R

---
+++ raw/makeMnPlas.R
@@ -0,0 +1,16 @@
+# this file makeMnPlas.R
+
+rm(list = ls())
+
+# read files ####
+mm <- read.csv("raw/MNTaxaImproved.csv")
+gg <- read.csv("raw/genusFamilyImproved.csv")
+
+# merge 1 ####
+
+# merge 2 ####
+
+
+
+# write file ####
+# write.csv(xx, "mnPlas20250603.csv", row.names = FALSE)
