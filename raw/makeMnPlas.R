6b5be27776f2edeb7632a2fc60ed7562e4e7909c
stuartWagenius <stuart.wagenius@gmail.com>
2025-06-06 20:55:01 -0500

    update makeMnPlas.R

--- raw/makeMnPlas.R
+++ raw/makeMnPlas.R
@@ -15,13 +15,18 @@ head(pp)
 
 aggregate(nameDNR ~ family, pp, length)
 
-
+# add speciesId & rename 2 cols
+pp$speciesId <- 1001:3373
+names(pp)[names(pp) == "genus_id"] <- 'genusId'
+names(pp)[names(pp) == "family_id"] <- 'familyId'
+  
 # write file ####
 # dput(names(pp))
 nama <- c("native",           "physiognomy", "synonymGC", "commonName", "nameDNR")
-namz <- c("native", "family", "physiognomy", "synonymGC", "commonName", "nameDNR")
+namz <- c("native", "family", "genus", "familyId", "genusId", "physiognomy", "synonymGC", "commonName", "nameDNR", "speciesId")
 
 
 # write.csv(pp[pp$family_id %in% 5, nama], "mnAsters20250603.csv", row.names = FALSE)
-# write.csv(pp[                 , namz],   "mnPlas20250603.csv", row.names = FALSE)
+# write.csv(pp[                 , namz],   "mnPlas20250606.csv", row.names = FALSE)
+# write.csv(pp[                 , namz],           "mnPlas.csv", row.names = FALSE)
 # write.csv(pp[pp$family_id %in% NA, c("genus", namz)], "mnProbs20250604.csv", row.names = FALSE)
